0477e1bbdbf42bee3f783c57a839d231804ec6c7
lpearson11101 <lpearson11101@gmail.com>
2026-05-21 17:21:19 -0400

    Updated routing code and changed my user account (I forgot my password)

--- app/books/routes.py
+++ app/books/routes.py
@@ -1,7 +1,20 @@
-from flask import render_template
+from flask import render_template, request, url_for, redirect
 from app.books import bp
+from app.models.book import Book
+from app.extensions import db
 
-@bp.route('/')
+
+@bp.route('/', methods=('GET', 'POST'))
 def index():
-    return render_template('books/index.html')
+    books = Book.query.all()
+
+    if request.method == 'POST':
+        new_book = Book(content=request.form['book'],
+                        answer=request.form['information'])
+        db.session.add(new_book)
+        db.session.commit()
+        return redirect(url_for('books.index'))
+    
+    return render_template('books/index.html', books=books)
+
 
